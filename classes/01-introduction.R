24760de5e35b459a9b85ed941ef3042f70664d3a
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-07-02 09:15:12 +0200

    added intro

---
+++ classes/01-introduction.R
@@ -0,0 +1,8 @@
+set.seed(1410)
+x <- factor(sample(letters[1L:4], 2000, replace = TRUE))
+
+# Napisz: 
+# - funkcję almost_summary(x), która wyświetla podsumowanie obiektu x 
+# (ile elementów a, b, c, djest w obiekcie) 
+# - funkcję almost_print(x, n = 10), która wyświetla pierwsze n 
+# elementow z obiektu x
