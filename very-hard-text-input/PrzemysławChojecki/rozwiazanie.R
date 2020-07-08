0eb82cbc5568c36a0710e98b591cc349a84bc3f1
DTGT <themegatgt48@gmail.com>
2020-07-08 14:46:12 +0200

    Dodanie funkcjonalnosci do rozwiazania

--- very-hard-text-input/PrzemysławChojecki/rozwiazanie.R
+++ very-hard-text-input/PrzemysławChojecki/rozwiazanie.R
@@ -1,10 +1,14 @@
 library(shiny)
+library(magrittr)
+library(shinyhelper)
 
 ui <- fluidPage(
   uiOutput("text")
 )
 
 server <- function(input, output, session) {
+  observe_helpers()
+  
   rv <- reactiveValues(kolor = "")
   
   observeEvent(input[["hexCol"]], {
@@ -17,10 +21,23 @@ server <- function(input, output, session) {
     rv[["kolor"]] <- kolor
   })
   
-  output[["text"]] <- renderUI({column(3, wellPanel(div(id='my_textinput' ,
-                          textInput ('hexCol', label = "Podaj kolor", value = "", width = "200px")),
-                      tags$style(type="text/css",
-                                 paste0("#hexCol {background-color: #", rv[["kolor"]], "}"))))
+  output[["text"]] <- renderUI({
+    
+    # sprawdzenie, czy podaje hex("d0d0d0"), czy nazwe ("red")
+    czy_hex <- rv[["kolor"]] %>% stringr::str_detect("^[0-9a-fA-F]{6}$")
+    kolor <- ifelse(czy_hex, "#", "") %>% paste0(rv[["kolor"]])
+    CSS <- paste0("#hexCol {background-color: ", kolor, "}")
+    
+    column(3, wellPanel(div(id='my_textinput' ,
+                          helper(textInput('hexCol', label = "Podaj kolor", value = "", width = "200px"),
+                                 type = "inline",
+                                 title = "Jak żyć?",
+                                 content = c("Można wpisać:",
+                                             "<ul>",
+                                                "<li>Red</li>",
+                                                "<li>d0d0d0</li>",
+                                             "</ul>"))),
+                      tags$style(type="text/css", CSS)))
   })
   
   observe({
